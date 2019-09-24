882494af2c1b814609851b8ef6b691a2e1035afd
ChangJoo Park <pcjpcj2@gmail.com>
2019-09-24 12:38:36 +0900

    기본 뼈대 완료

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -25,7 +24,33 @@ class BLETesterPage extends StatefulWidget {
 }
 
 class _BLETestertate extends State<BLETesterPage> {
+  List<Map> items = [];
   bool isDiscoverMode = true;
+
+  @override
+  void initState() {
+    items.add(
+      {
+        "name": "Advertise Mode",
+        "route": () {
+          Route route =
+              MaterialPageRoute(builder: (context) => AdvertiseModePage());
+          Navigator.push(context, route);
+        }
+      },
+    );
+
+    items.add({
+      "name": "Discover Mode",
+      "route": () {
+        Route route =
+            MaterialPageRoute(builder: (context) => DiscoverModePage());
+        Navigator.push(context, route);
+      }
+    });
+
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,11 +61,69 @@ class _BLETestertate extends State<BLETesterPage> {
           itemCount: 2,
           itemBuilder: (context, index) {
             return ListTile(
-              title: Text('Mode'),
+              title: Text(items[index]["name"]),
+              onTap: () => items[index]["route"](),
             );
           },
         ),
       ),
     );
   }
+}
+
+class DiscoverModePage extends StatefulWidget {
+  @override
+  _DiscoverModePageState createState() => _DiscoverModePageState();
+}
+
+class _DiscoverModePageState extends State<DiscoverModePage> {
+  bool _started = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Discover Mode")),
+      body: Container(),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(_started ? Icons.star : Icons.star_border),
+        onPressed: () {
+          setState(() => _started = !_started);
+          _handleMode(_started);
+        },
+      ),
+    );
+  }
+
+  _handleMode(bool needStart) => needStart ? start() : stop();
+
+  start() {}
+  stop() {}
+}
+
+class AdvertiseModePage extends StatefulWidget {
+  @override
+  _AdvertiseModePageState createState() => _AdvertiseModePageState();
+}
+
+class _AdvertiseModePageState extends State<AdvertiseModePage> {
+  bool _started = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Advertise Mode")),
+      body: Container(),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(_started ? Icons.star : Icons.star_border),
+        onPressed: () {
+          setState(() => _started = !_started);
+          _handleMode(_started);
+        },
+      ),
+    );
+  }
+
+  _handleMode(bool needStart) => needStart ? start() : stop();
+
+  start() {}
+  stop() {}
 }
